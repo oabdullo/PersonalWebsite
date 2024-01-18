5d1d8d639e90aa1b9193bc0c337c7a550e704830
Abdullo Omonullaev <abdulloomonullaev@Abdullos-MBP.lan>
2024-01-18 15:16:28 -0600

    added a small logo to the website

--- src/content_option.js
+++ src/content_option.js
@@ -107,60 +107,15 @@ const dataportfolio = [{
         link: "https://github.com/QuizPop/main",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
         img: "https://i.imghippo.com/files/1705465881.png",
         description: "Personal Website Project",
         link: "https://github.com/oabdullo/personalReact",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
         img: "https://i.imghippo.com/files/1705466069.png",
         description: "Data Visualization using Numpy",
         link: "https://github.com/oabdullo/CSE332",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    }
 ];
 
 const dataCars = [{
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -40,8 +40,16 @@ export const Home = () => {
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn ">
+                    <div id="button_h" className="ac_btn btn ">
                       My Portfolio
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                  <Link to="/cars" className="text_2">
+                    <div id="button_h" className="ac_btn btn ">
+                      My Cars
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
